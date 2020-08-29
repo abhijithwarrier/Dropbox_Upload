e814ca1225311afbc9d0ff0a8e1d266aecdfcfe1
ABHIJITH WARRIER <46685919+abhijithwarrier@users.noreply.github.com>
2020-08-30 00:05:53 +0530

    Add files via upload

--- Dropbox_Upload.py
+++ Dropbox_Upload.py
@@ -2,11 +2,25 @@
 
 # A PYTHON SCRIPT TO UPLOAD FILES TO DROPBOX USING dropbox MODULE
 
+# dropbox Module is Python SDK for integrating with Dropbox API v2. Compatible with Python 2.7 & 3.4+
+# The module can be installed using the command - pip install dropbox
+
+# To use Dropbox API, a new app has to be registered in Dropbox App Console. Follow the below steps
+# 1 - Login to the following link - https://www.dropbox.com/developers/apps
+# 2 - After signing in click on Create App.
+# 3 - In the next page make the following selections
+#   1. Choose an API
+#       Select Dropbox API
+#   2. Choose the type of access you need
+#       Select Full Dropbox – Access to all files and folders in a user's Dropbox.
+#   3. Name your app
+#       Give a name to your APP
+# 4 - Click on Create App
+# 5 - In the next page, click on Generate, to generate ACCESS TOKEN
+
 # Importing the necessary packages
 import os
 import dropbox
-
-# ---
 
 # Defining Dropbox_Upload() with the user-input file as the input token
 def Dropbox_Upload(input_file):
@@ -47,5 +61,3 @@ if __name__ == '__main__':
             # Calling the Dropbox_Upload with the user-list file as the argument
             Dropbox_Upload(file_list)
             break
-
-# ---
